14229dac354ac5f8394b6d0e829d37561d24daa7
Hugo Duksis <duksis@gmail.com>
2016-02-17 23:44:13 +0100

    Adds dependencies

--- mix.exs
+++ mix.exs
@@ -17,16 +17,12 @@ defmodule TravisEx.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.8"},
+      {:poison, "~> 1.5"},
+      {:exvcr, "~> 0.6", only: :test},
+      {:excoveralls, "~> 0.4", only: :test}
+    ]
   end
 end
